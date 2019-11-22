a5714e698e9498520a0d6207092135b4129f5f6c
manuelwecker <manuelwecker@web.de>
2019-11-22 18:54:58 +0100

    Add headline, input fields, submit button and result div to homepage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <header>Header</header>
-      <aside>Input-Latt, Input-Long, Submit</aside>
-      <main>Output</main>
+      <header>
+        <h1>Is a storm coming?</h1>
+      </header>
+      <aside>
+        <input
+          type="number"
+          class="search__latt"
+          value="51"
+          placeholder="Please enter Latitude"
+        />
+        <input
+          type="number"
+          class="search__long"
+          value="7"
+          placeholder="Please enter Longitude"
+        />
+        <button class="search__submit">Submit your position</button>
+      </aside>
+      <main>
+        <div class="results">Output from API</div>
+      </main>
     </>
   );
 }
